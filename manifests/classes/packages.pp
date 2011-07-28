2c381be1c3e89d260013f9d045cb80b304738d27
Dave Simons <dave@inuits.be>
2011-07-28 11:52:17 +0200

    add backends, unvirt solr
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -1,5 +1,12 @@
 class solr::packages {
     package {
+		'solr':
+			ensure => present,
+			name => $operatingsystem ? {
+				Centos => 'solr',
+				Debian => 'solr-common',
+			};
+
 		'java':
 			ensure => present,
 			name => $operatingsystem ? {
@@ -7,14 +14,27 @@ class solr::packages {
 					'6.0' => "java-1.6.0-openjdk.$hardwaremodel",
 					'*' => 'openjdk-6-jre',
 				},
-				'*' => 'openjdk-6-jre',
+				'Debian' => 'openjdk-6-jre-headless',
 			};
+	}
 
-		'solr':
-			ensure => present;
-
+	@package {
 		'tomcat':
 			ensure => present,
 			name => 'tomcat6';
+
+		'jetty':
+			ensure => present,
+			name => 'jetty';
+
+		'solr-tomcat':
+			ensure => present;
+
+		'solr-jetty':
+			ensure => present;
+	}
+
+	if $operatingsystem == Debian {
+		realize(Package["solr-$solr::backend"], Aptrepo['unstable'])
 	}
 }

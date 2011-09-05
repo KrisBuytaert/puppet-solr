9a18e6b1477ce499adc2c7e9f0004b84a7cc2b2d
Dave Simons <dave@inuits.be>
2011-09-05 17:49:30 +0200

    add solr::source class with template and use it on debian
    Signed-off-by: Dave Simons <dave@inuits.be>

---
+++ manifests/classes/source.pp
@@ -0,0 +1,32 @@
+class solr::source {
+	exec {
+		'grab_solr_source':
+			command => 'wget http://apache.belnet.be//lucene/solr/3.3.0/apache-solr-3.3.0.tgz',
+			path => '/usr/bin',
+			creates => '/usr/share/apache-solr-3.3.0.tgz',
+			cwd => '/usr/share';
+
+		'unpack_solr_source':
+			command => 'tar xzvf apache-solr-3.3.0.tgz',
+			path => '/bin',
+			creates => '/usr/share/solr',
+			cwd => '/usr/share',
+			require => Exec['grab_solr_source'];
+
+		'install_solr_into_tomcat':
+			command => 'cp /usr/share/apache-solr-3.3.0/dist/apache-solr-3.3.0.war /var/lib/tomcat6/webapps/solr.war',
+			creates => '/var/lib/tomcat6/webapps/solr.war',
+			require => Exec['unpack_solr_source'];
+	}
+
+	file {
+		'tomcat6.default':
+			ensure => present,
+			path => '/etc/default/tomcat6',
+			owner => root,
+			group => root,
+			mode => 0644,
+			content => template('solr/tomcat6.default.erb'),
+			require => Package['tomcat'];
+	}
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,19 @@ class solr (
 	$backend = 'tomcat'
 ) {
 	class {
-		'solr::packages':
-			before => Class['solr::config'];
 		'solr::config':
 			before => Class['solr::services'];
 		'solr::services':;
+	}
+	if $::operatingsystem == debian {
+		class {
+			'solr::source':
+				before => Class['solr::config'];
+		}
+	} else {
+		class {
+			'solr::packages':
+				before => Class['solr::config'];
+		}
 	}
 }

17b05a90daf376ea64d80722848df0074554d84a
Dave Simons <dave@inuits.be>
2011-09-08 14:44:16 +0200

    add tomcat service, disable solr service
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/services.pp
+++ manifests/classes/services.pp
@@ -1,8 +1,13 @@
 class solr::services {
 	service {
 		'solr':
-			ensure => "running",
+			ensure => "stopped",
 			enable => "true",
 			require => File['solr initscript'];
+
+		'tomcat6':
+			ensure => running,
+			enable => true,
+			require => File['tomcat6.default'];
     }
 }
